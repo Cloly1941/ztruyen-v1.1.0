55efad4f2b6321df31d4b01af3c67638a03fe0c9
MAY-06\Administrator <trongbut03@gmail.com>
2026-06-28 15:22:05 +0700

    fix(ui): image full screen

--- src/app/(auth)/dang-nhap/page.tsx
+++ src/app/(auth)/dang-nhap/page.tsx
@@ -1,15 +1,15 @@
 // ** Next
 import Link from "next/link";
-import {Metadata} from "next";
+import { Metadata } from "next";
 
 // ** Lucide Icon
-import {Home} from "lucide-react";
+import { Home } from "lucide-react";
 
 // ** Components
 import AuthFooter from "@/components/auth/AuthFooter";
 import HandleSocialToken from "@/components/auth/HandleSocialToken";
 import GuestGuard from "@/components/guards/GuestGuard";
-import {ListenOAuthMessage} from "@/components/auth/ListenOAuthMessage";
+import { ListenOAuthMessage } from "@/components/auth/ListenOAuthMessage";
 
 // ** Modules
 import FormLogin from "@/modules/dang-nhap/FormLogin";
@@ -44,26 +44,26 @@ export const metadata: Metadata = {
 
 type TLoginSearchParams = { token?: string }
 
-const Login = async ({searchParams}: { searchParams: Promise<TLoginSearchParams> }) => {
+const Login = async ({ searchParams }: { searchParams: Promise<TLoginSearchParams> }) => {
 
     const { token } = await searchParams
 
     return (
         <GuestGuard>
-            <div className='bg-auth flex justify-center items-center p-10 bg-black/40 dark:bg-white/40'>
+            <div className='bg-auth h-screen flex justify-center items-center p-10 bg-black/40 dark:bg-white/40'>
                 <HandleSocialToken token={token} />
-                <ListenOAuthMessage/>
+                <ListenOAuthMessage />
                 <div>
                     <div className='flex justify-center mb-5'>
                         <Link href='/' className='hover:scale-105'>
-                            <Home className='text-white size-6'/>
+                            <Home className='text-white size-6' />
                         </Link>
                     </div>
                     <div
                         className='bg-background rounded-lg pt-10 pb-6 px-6 sm:px-14 lg:px-16 flex flex-col justify-center items-center'>
                         <h1 className='text-primary capitalize font-semibold text-base lg:text-lg dark:text-white'>Đăng nhập</h1>
-                        <FormLogin/>
-                        <AuthFooter/>
+                        <FormLogin />
+                        <AuthFooter />
                         <div className="mt-4 text-center text-sm text-muted-foreground">
                             Chưa có tài khoản?
                             <Link
